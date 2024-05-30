86e1a80dbc130577437314b3c03a93984b9f027b
jonasfelk <felkjonas@gmail.com>
2024-05-30 22:26:30 +0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { createClient } from '@/utils/supabase/server'
-import Image from 'next/image'
 export const runtime = 'edge'
 export default async function Home() {
   const supabase = createClient()
@@ -9,7 +8,7 @@ export default async function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-6xl font-bold'>Hello World</h1>
+      <h1 className='text-6xl font-bold'>Hello World!</h1>
       {notes?.map((note) => (
         <p
           className='text-2xl text-white'
