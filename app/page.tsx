ffea7a47078f5252e989f806613ecf884c05aa37
jonasfelk <felkjonas@gmail.com>
2024-06-01 23:38:41 +0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-6xl font-bold'>Hello World</h1>
+      <h1 className='text-6xl font-bold'>Hello World1</h1>
       {notes?.map((note) => (
         <p
           className='text-2xl text-white'
